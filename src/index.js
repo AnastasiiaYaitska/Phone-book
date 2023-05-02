a83f5bf9142e8c97c58761719f1a47a9ae1ff837
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-05-02 15:48:24 +0200

    change basename

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter
-          basename="phone-book
-        "
-        >
+        <BrowserRouter basename="phone-book">
           <App />
         </BrowserRouter>
       </PersistGate>
